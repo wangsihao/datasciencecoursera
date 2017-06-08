860a3f0e16a78c08db23dbab2aa31b13d33d188c
wangsihao <wangsihao@gmail.com>
2017-06-08 10:20:30 -0500

    Update test3.py

--- For_SW_HOC_inRGC/For_SW_HOC_inRGC/test3.py
+++ For_SW_HOC_inRGC/For_SW_HOC_inRGC/test3.py
@@ -76,7 +76,7 @@ mu, sigma = noise_params(0., 1., dt=dt)
 #print "mu = {:.2f}, sigma = {:.2f}".format(mu, sigma)
 
 t, s, v = simulate(mu, sigma, dt=dt)
-plt.plot(t,v,'--',color='blue',label='dt = 0.3')
+plt.plot(t,v,'.',color='blue',label='dt = 0.3')
 plt.legend()
 
 plt.show()
