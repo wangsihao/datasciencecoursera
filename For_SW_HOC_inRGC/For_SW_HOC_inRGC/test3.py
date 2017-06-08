b8b4d2c6d3bfbcb44c3eb6731506fd2145329452
wangsihao <wangsihao@gmail.com>
2017-06-08 12:10:08 -0500

    Create test3.py

--- For_SW_HOC_inRGC/For_SW_HOC_inRGC/test3.py
+++ For_SW_HOC_inRGC/For_SW_HOC_inRGC/test3.py
@@ -14,30 +14,10 @@ def noise_params(V_mean, V_std, dt=1.0, tau_m=10., C_m=250.):
     
     return C_m / tau_m * V_mean, math.sqrt(2/(tau_m*dt))*C_m*V_std
 
-def V_asymptotic(mu, sigma, dt=1.0, tau_m=10., C_m=250.):
-    'Returns asymptotic mean and std of V_m'
-    
-    V_mean = mu * tau_m / C_m
-    V_std = (sigma * tau_m / C_m) * np.sqrt(( 1 - math.exp(-dt/tau_m) ) / ( 1 + math.exp(-dt/tau_m) ))
-                                    
-    return V_mean, V_std
-  
-def V_mean(t, mu, tau_m=10., C_m=250.):
-    'Returns predicted voltage for given times and parameters.'
-    
-    vm, _ = V_asymptotic(mu, sigma, tau_m=tau_m, C_m=C_m)
-    return vm * ( 1 - np.exp( - t / tau_m ) )
-
-def V_std(t, sigma, dt=1.0, tau_m=10., C_m=250.):
-    'Returns predicted variance for given times and parameters.'
-    
-    _, vms = V_asymptotic(mu, sigma, dt=dt, tau_m=tau_m, C_m=C_m)
-    return vms * np.sqrt(1 - np.exp(-2*t/tau_m))
-  
 
 
 
-def simulate(mu, sigma, dt=1.0, tau_m=10., C_m=250., N=1000, t_max=50.):
+def simulate(mu, sigma, dt=1.0, t_max=50.):
 
     
     resolution = 0.001
