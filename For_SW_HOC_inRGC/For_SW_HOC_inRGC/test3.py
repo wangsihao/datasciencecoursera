37f646ff5d8657f36600411715eb6410d0111a3d
wangsihao <wangsihao@gmail.com>
2017-06-08 12:08:27 -0500

    Create test3.py

--- For_SW_HOC_inRGC/For_SW_HOC_inRGC/test3.py
+++ For_SW_HOC_inRGC/For_SW_HOC_inRGC/test3.py
@@ -65,7 +65,7 @@ mu, sigma = noise_params(0., 1., dt=dt)
 #print "mu = {:.2f}, sigma = {:.2f}".format(mu, sigma)
 
 #t, s, v = simulate(mu, sigma, dt=dt)
-t, s, v = simulate(0, 1, dt=dt)
+t, s, v = simulate(0., 1., dt=dt)
 plt.plot(t,v,'--',color='red',label='dt = 0.1')
 
 
@@ -75,7 +75,7 @@ mu, sigma = noise_params(0., 1., dt=dt)
 #print "mu = {:.2f}, sigma = {:.2f}".format(mu, sigma)
 
 #t, s, v = simulate(mu, sigma, dt=dt)
-t, s, v = simulate(0, 10, dt=dt)
+t, s, v = simulate(0., 10., dt=dt)
 plt.plot(t,v,'--',color='blue',label='dt = 0.001')
 
 plt.legend()
